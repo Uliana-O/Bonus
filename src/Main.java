a2c87f542ee8281b6ed8fc8e4cce0c77d02ea04c
Uliana <uosintseva@gmail.com>
2023-01-30 20:19:43 +0300

    add files

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int bonus;
+        int account = 500;
+        int deposit = 1500;
+            if (deposit > 1000)
+        {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Итоговый счет:"+(account+deposit+bonus)+" "+ "р");
+        System.out.println("в том числе бонусных рублей:"+bonus);
+
+    }
+}
