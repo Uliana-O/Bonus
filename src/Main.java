74e2f7972a7d70fdc4405b33dade4e37372714ad
Uliana <uosintseva@gmail.com>
2023-01-31 10:10:34 +0300

    change file

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,13 @@ public class Main {
         int bonus;
         int account = 500;
         int deposit = 1500;
-            if (deposit > 1000)
-        {
+        if (deposit > 1000) {
             bonus = deposit / 100;
         } else {
             bonus = 0;
         }
-        System.out.println("Итоговый счет:"+(account+deposit+bonus)+" "+ "р");
-        System.out.println("в том числе бонусных рублей:"+bonus);
+        System.out.println("Итоговый счет:" + (account + deposit + bonus) + " " + "р");
+        System.out.println("в том числе бонусных рублей:" + bonus);
 
     }
 }
